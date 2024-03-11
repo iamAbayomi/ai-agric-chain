eab963edcb83b66ae8961dd1d10e35fdeed0f043
Muhammed Jimoh <m.jimoh@getkole.co>
2024-03-11 09:35:30 +0100

    feat: setup supply chain

--- backend/apps/apps/settings.py
+++ backend/apps/apps/settings.py
@@ -39,6 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users',
+    'farmer',
+    'processor',
+    'product',
+    'retailer',
+    'supply_chain',
 ]
 
 MIDDLEWARE = [
